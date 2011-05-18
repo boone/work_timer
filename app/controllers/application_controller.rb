cc094b02fcc3f7993ba03744620e1bfabb006675
Mike Boone <mike@boonedocks.net>
2011-05-18 08:21:04 -0400

    Added menu and current event display.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def current_events
+    @current_events = Event.current
+  end
+  helper_method :current_events
 end
