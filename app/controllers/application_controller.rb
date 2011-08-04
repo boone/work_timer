3ab3125f4b338d3aa74a3844a4830beb3ef01b49
Mike Boone <mike@boonedocks.net>
2011-08-04 10:23:40 -0400

    Added handling of unverified requests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  def handle_unverified_request
+    raise ActionController::InvalidAuthenticityToken
+  end
+  
   def current_events
     @current_events = Event.current
   end
