48552a7acdda903ea63c8ed635224530d8c14fc4
Mike Boone <mike@boonedocks.net>
2011-10-23 16:49:03 -0400

    Show current time in title, update via Ajax.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,8 @@
 $(function() {
   setInterval(function() {
-    $.get('events/current', function(data) {
+    $.get('/events/current', function(data) {
       $('#status').html(data);
+      if ($('.time')) document.title = $('.time').html() + '- Work Timer';
     });
   }, 30000);
   if ($('#flash').length > 0) {
